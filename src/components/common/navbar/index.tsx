fca30037190f2d78efefd437b0eb7a3771f2223b
Rowmel Marco Dabon <rowmel@ingenuity.ph>
2025-01-23 20:03:05 +0800

    feat: update theming

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -1,26 +1,26 @@
-import { ModeToggle } from '@/components/theme/toggler';
+import { ModeToggle } from "@/components/theme/toggler";
 
 export const Navbar = () => (
-  <header className='bg-background border-b border-b-primary py-4'>
-    <div className='container mx-auto flex items-center justify-between px-4'>
+  <header className="bg-navbar py-4 shadow-md">
+    <div className="container mx-auto flex items-center justify-between px-4">
       {/* Logo */}
-      <h1 className='text-xl font-bold'>funance.</h1>
+      <h1 className="text-xl font-bold">funance.</h1>
 
       {/* Navigation */}
       <nav>
-        <ul className='flex space-x-4 items-center'>
+        <ul className="flex space-x-4 items-center">
           <li>
-            <a href='#' className='hover:underline'>
+            <a href="#" className="hover:underline">
               Home
             </a>
           </li>
           <li>
-            <a href='#' className='hover:underline'>
+            <a href="#" className="hover:underline">
               Transactions
             </a>
           </li>
           <li>
-            <a href='#' className='hover:underline'>
+            <a href="#" className="hover:underline">
               Settings
             </a>
           </li>
